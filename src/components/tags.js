d5e3d99e217e1ef7a8d3e950fc4f757f9d76bd84
feder1208 <fedesanchezdt@hotmail.com>
2022-09-12 16:17:33 -0300

    cambios en filtros

--- src/components/tags.js
+++ src/components/tags.js
@@ -5,16 +5,45 @@ function Tags({ articles }) {
 
   const tags = filterArticles.map((article) => {
     return article.taxonomy.tags;
-  });
-  console.log(tags, "en tags");
+  }).flat();
+//   console.log(tags, "en tags");
 
-  for (let text of tags) {
-    console.log(text);
+
+
+  function groupTags(obj, prop) {
+
+    return obj.reduce((acc, curr) =>  {
+    
+        
+      let key = curr[prop]
+  
+      if (!acc[key]) {
+  
+        acc[key] = []
+        
+  
+      }
+  
+      acc[key].push(curr)
+  
+      return acc
+  
+    }, {})
+  
+  }
+  
+  let groupedTags = groupTags(tags, 'text')
+  
+  console.log(groupedTags, 'groupedTags')
+
+
+ /*  for (let text of tags) {
+    console.log(text.text);
     for (let final of text) {
         console.log(final.text);
     } 
   }
-
+ */
   return (
     <>
       <div>tags</div>
