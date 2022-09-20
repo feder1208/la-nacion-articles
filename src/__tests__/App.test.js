7ef321e4ea707bcaad52592f664c1912448fe973
feder1208 <fedesanchezdt@hotmail.com>
2022-09-20 11:32:08 -0300

    en styles.css se agrega un padding en el footer de las redes, para que en mobile se vean correctamente. Se crea el primer test para evaluar la función de fetchData, y pasa correctamente

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,8 +3,16 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../App";
 
-test("App component test", () => {
-    const {container, debug} = render(<App />)
-    // debug();
-  expect(container).toMatchSnapshot();
-});
+// test("App component test", () => {
+//     const {container, debug} = render(<App />)
+//     // debug();
+//   expect(container).toMatchSnapshot();
+// });
+
+// function sum(a, b) {
+//     return a + b;
+//   }
+
+//   test('adds 1 + 2 to equal 3', () => {
+//     expect(sum(1, 2)).toBe(3);
+//   });  
